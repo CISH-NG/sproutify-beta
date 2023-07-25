7a188632218d4f7692da242196ce0a9a151f89d7
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-07-25 11:38:18 +0100

    WIP-Implementing Navbar

---
+++ src/assets/svg/addIcon.tsx
@@ -0,0 +1,36 @@
+const AddIcon = () => {
+	return (
+		<svg
+			width="40"
+			height="40"
+			viewBox="0 0 40 40"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<g clipPath="url(#clip0_54_896)">
+				<rect width="40" height="40" rx="20" fill="#00820D" />
+				<g clipPath="url(#clip1_54_896)">
+					<path
+						d="M26 21H21V26C21 26.55 20.55 27 20 27C19.45 27 19 26.55 19 26V21H14C13.45 21 13 20.55 13 20C13 19.45 13.45 19 14 19H19V14C19 13.45 19.45 13 20 13C20.55 13 21 13.45 21 14V19H26C26.55 19 27 19.45 27 20C27 20.55 26.55 21 26 21Z"
+						fill="white"
+					/>
+				</g>
+			</g>
+			<defs>
+				<clipPath id="clip0_54_896">
+					<rect width="40" height="40" rx="20" fill="white" />
+				</clipPath>
+				<clipPath id="clip1_54_896">
+					<rect
+						width="24"
+						height="24"
+						fill="white"
+						transform="translate(8 8)"
+					/>
+				</clipPath>
+			</defs>
+		</svg>
+	);
+};
+
+export default AddIcon;
