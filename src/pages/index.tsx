5594a0a7fb60f467ac58b64ec5eb7aff247a0fee
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-08-03 15:04:59 +0100

    WIP: Implemeting Media responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,44 @@ const clientsSaying = [
   },
 ];
 
+const services = [
+  {
+    id: 1,
+    name: "Social Media",
+    content:
+      "Elevate your online presence with specifically crafted social media management strategies, that align with your brands voice and vision.",
+    image: phonelink,
+  },
+  {
+    id: 2,
+    name: "Website",
+    content:
+      " Say goodbye to complex coding and endless frustration. Sproutify provides you with a hassle-free website creation experience where you can focus on unleashing your creativity while we take care of the technical complexities.",
+    image: website,
+  },
+  {
+    id: 3,
+    name: "Digital Marketing",
+    content:
+      " Achieve online success with carefully curated digital marketing strategies tailored to your brands needs.",
+    image: digitalmarketing,
+  },
+  {
+    id: 4,
+    name: "Subscriptions",
+    content:
+      "Experience a range of Sproutify’s capabilities without compromising your budget. Whether you’re a budding entrepreneur or an established enterprise, our flexible pricing options ensure that you can choose a plan that aligns perfectly with your goals.",
+    image: subscriptions,
+  },
+  {
+    id: 5,
+    name: "Designs",
+    content:
+      "Bring your brand to life through our innovative and visually captivating designs that are sure to leave a lasting impression on your audience.",
+    image: designs,
+  },
+];
+
 export default function Home() {
   return (
     <DefaultLayouts>
@@ -85,93 +123,38 @@ export default function Home() {
           />
         </div>
       </section>
-
-      <section className="w-[1073px] h-[104px] flex justify-center items-center font-semibold bg-[#1F1717] mx-auto   my-20 rounded-xl text-center">
-        <h1 className="text-[#FFF] text-[40px]">
+      <section className="lg:max-w-[1073px] w-full h-[104px] flex justify-center items-center font-semibold bg-[#1F1717] mx-auto   my-20 rounded-xl text-center">
+        <h1 className="text-[#FFF] lg:text-[40px] text-[20px]">
           Over <span className="text-[#FEC431]">50,000</span> businesses
           enjoying tech services
         </h1>
       </section>
-
       <section className="w-full mb-20">
-        <div className="w-[400px] mx-auto text-center">
-          <h1 className="text-[32px] font-semibold text-[#1F1717]">
+        <div className="lg:w-[400px] w-full mx-auto text-center">
+          <h1 className="text-[30px] font-semibold text-[#1F1717]">
             We offer digital solutions for your business
           </h1>
         </div>
       </section>
-
-      <section className="w-[1200px] mx-auto flex p-5 justify-evenly flex-wrap gap-5 mb-20">
-        <div className="w-[340px] h-[400px]  flex flex-col justify-center items-center gap-5">
-          <div className="w-[80px] h-[80px] bg-[#CECECE40] rounded-full items-center flex justify-center">
-            {" "}
-            <Image src={phonelink} alt="" />
+      <section className="lg:w-[1200px] w-full lg:mx-auto flex lg:p-5 justify-evenly flex-col lg:flex-wrap gap-5 mb-20">
+        {services.map((service) => (
+          <div
+            key={service.id}
+            className="w-[340px] h-[400px]  flex flex-col justify-center items-center gap-5"
+          >
+            <div className="w-[80px] h-[80px] bg-[#CECECE40] rounded-full items-center flex justify-center">
+              {" "}
+              <Image src={service.image} alt="" />
+            </div>
+            <h1 className="text-[#1F1717] text-[24px] font-bold">
+              {service.name}
+            </h1>
+            <p className="text-[#A0A0A0] lg:text-[24px] text-[16px] text-center w-[280px]">
+              {service.content}
+            </p>
           </div>
-          <h1 className="text-[#1F1717] text-[24px] font-bold">Social Media</h1>
-          <p className="text-[#A0A0A0] text-[24px] text-center">
-            Elevate your online presence with specifically crafted social media
-            management strategies, that align with your brands voice and vision.
-          </p>
-        </div>
-
-        <div className="w-[340px] h-[500px]   flex flex-col justify-center items-center gap-5">
-          <div className="w-[80px] h-[80px] bg-[#CECECE40] rounded-full items-center flex justify-center">
-            {" "}
-            <Image src={website} alt="" />
-          </div>
-          <h1 className="text-[#1F1717] text-[24px] font-bold">Website</h1>
-          <p className="text-[#A0A0A0] text-[24px] text-center">
-            Say goodbye to complex coding and endless frustration. Sproutify
-            provides you with a hassle-free website creation experience where
-            you can focus on unleashing your creativity while we take care of
-            the technical complexities.
-          </p>
-        </div>
-
-        <div className="w-[340px] h-[350px]  flex flex-col justify-center items-center gap-5">
-          <div className="w-[80px] h-[80px] bg-[#CECECE40] rounded-full items-center flex justify-center">
-            {" "}
-            <Image src={digitalmarketing} alt="" />
-          </div>
-          <h1 className="text-[#1F1717] text-[24px] font-bold">
-            Digital Marketing{" "}
-          </h1>
-          <p className="text-[#A0A0A0] text-[24px] text-center">
-            Achieve online success with carefully curated digital marketing
-            strategies tailored to your brands needs.
-          </p>
-        </div>
-
-        <div className="w-[340px] h-[500px] flex flex-col justify-center items-center gap-5">
-          <div className="w-[80px] h-[80px] bg-[#CECECE40] rounded-full items-center flex justify-center">
-            {" "}
-            <Image src={subscriptions} alt="" />
-          </div>
-          <h1 className="text-[#1F1717] text-[24px] font-bold">
-            Subscriptions{" "}
-          </h1>
-          <p className="text-[#A0A0A0] text-[24px] text-center">
-            Experience a range of Sproutify’s capabilities without compromising
-            your budget. Whether you’re a budding entrepreneur or an established
-            enterprise, our flexible pricing options ensure that you can choose
-            a plan that aligns perfectly with your goals.
-          </p>
-        </div>
-
-        <div className="w-[340px] h-[350px]  flex flex-col justify-center items-center gap-5">
-          <div className="w-[80px] h-[80px] bg-[#CECECE40] rounded-full items-center flex justify-center">
-            {" "}
-            <Image src={designs} alt="" />
-          </div>
-          <h1 className="text-[#1F1717] text-[24px] font-bold">Designs </h1>
-          <p className="text-[#A0A0A0] text-[24px] text-center">
-            Bring your brand to life through our innovative and visually
-            captivating designs that are sure to leave a lasting impression on
-            your audience.
-          </p>
-        </div>
+        ))}
       </section>
-
       <section className=" relative my-12 flex h-[100px] w-full justify-around gap-5 bg-[#4C2884] lg:mt-52 lg:h-[320px] ">
         <div className="absolute top-0 left-0 z-0 rotate-0" aria-hidden="true">
           <Polygon />
@@ -196,43 +179,47 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="w-full h-[478px] flex my-[200px] ">
-        <div className="flex-1 bg-[#1F1717] text-[#FFFFFF] flex flex-col p-10 gap-8">
-          <h1 className="text-[32px] w-[518px] font-semibold mx-48">
+      <section className="w-full lg:h-[478px] h-[500px] flex lg:flex-row flex-col my-[200px] ">
+        <div className="lg:flex-1 bg-[#1F1717] w-full text-[#FFFFFF] flex flex-col p-3 lg:p-10 gap-8">
+          <h1 className="lg:text-[32px] text-[24px] text-center lg:w-[518px] w-full font-semibold lg:mx-48">
             Create quick invoices for your customers
           </h1>
-          <p className="text-[24px] text-[#A0A0A0] w-[800px] font-semibold mx-48">
+          <p className="lg:text-[24px] text-[16px] text-[#A0A0A0] lg:w-[800px] w-full text-center font-semibold lg:mx-48">
             Streamline your invoicing process and save time with our quick
             invoicing solution. Easily generate professional invoices for your
             customers with just a few clicks. Say goodbye to manual invoicing
             and hello to fast, efficient billing. Try our quick invoicing tool
             today and get paid faster!
           </p>
-          <button className="mx-48 bg-[#4C2884] w-[230px] px-[13px] py-[20px] text-[24px] rounded-xl">
+          <button className="lg:mx-48 mb-5 bg-[#4C2884] lg:w-[230px] lg:px-[13px] px-[6px] py-[10px] text-[16px] lg:py-[20px] lg:text-[24px] rounded-xl">
             Create Invoice
           </button>
         </div>
-        <div className="w-[478px]">
+        <div className="lg:w-[478px] w-full">
           <Image src={retailer2} alt="" />
         </div>
       </section>
 
-      <section className="flex flex-col my-[180px] gap-[180px] ">
+      <section className="flex flex-col lg:my-[180px] mt-[320px] gap-[180px] ">
         <div className="text-center ">
           <h1 className="text-[32px] font-semibold">
             What Our Clients are saying
           </h1>
         </div>
 
-        <div className="w-[1200px] mx-[400px] flex flex-wrap gap-10 ">
+        <div className="lg:w-[1200px] w-full lg:mx-[400px] flex flex-wrap gap-10 p-3 ">
           {clientsSaying.map((client) => (
-            <div className=" w-[45%] flex gap-10" key={client.id}>
-              <div className="w-[120px] h-[122px]">
+            <div className=" lg:w-[45%] w-full flex gap-10" key={client.id}>
+              <div className="lg:w-[120px] lg:h-[122px] w-[60px] h-[60px]">
                 <Image src={client.image} alt="" />
               </div>
-              <div className="w-[321px] flex flex-col">
-                <h1 className="text-[32px] font-semibold">{client.name}</h1>
-                <p className="text-[#A0A0A0] text-[24px]">{client.comment}</p>
+              <div className="lg:w-[321px] w-full flex flex-col">
+                <h1 className="lg:text-[32px] text-[24px] font-semibold">
+                  {client.name}
+                </h1>
+                <p className="text-[#A0A0A0] lg:text-[24px] text-[16px]">
+                  {client.comment}
+                </p>
               </div>
             </div>
           ))}
@@ -252,15 +239,15 @@ export default function Home() {
       </section>
 
       <section className="mx-auto flex flex-col gap-6 justify-center items-center my-[126px] ">
-        <h1 className="text-[24px] text-[#1F1717] font-normal">
+        <h1 className="text-[24px] text-[#1F1717] text-center font-normal">
           Subscribe to get prompt updates
         </h1>
-        <div className="flex justify-center">
+        <div className="flex justify-center ">
           <div className="relative">
             <input
               type="text"
               placeholder="Email Address"
-              className="py-4 px-2 pl-14 pr-12 w-[420px] bg-[#9A4CEC] rounded-lg focus:outline-none "
+              className="py-4 px-2 pl-14 pr-12 w-full lg:w-[420px] bg-[#9A4CEC] rounded-lg focus:outline-none "
             />
             <button className="absolute right-1 top-1/2 transform -translate-y-1/2 h-12 w-[120px] bg-[#4C2884] text-white text-[18px] rounded-lg flex items-center justify-center focus:outline-none">
               Subscribe
