f1e447b056a86126708f945ac721b5d97bd664c4
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-08-12 18:21:53 +0100

    Implementing signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import googleBtn from "../assets/svg/google-btn.svg";
 import facebookBtn from "../assets/svg/facebook-btn.svg";
 import Navbar from "@/components/NavBar/NavBar";
+import Polygon4 from "@/assets/images/Polygon4.png";
 
 function Signin() {
   const [email, setEmail] = useState("");
@@ -31,35 +32,20 @@ function Signin() {
   return (
     <>
       <Navbar />
-      <div className="flex gap-64">
-        <div className="w-[800px] flex items-center justify-end text-[#FFFFFF]">
-          <div className="h-full w-full bg-[#4C2884] rounded-tr-[50%] rounded-br-[50%] flex items-center justify-center">
-            <div className="flex flex-col items-center justify-center gap-[20px]">
-              <h1 className="text-[24px] font-medium">
-                You do not have an account?
-              </h1>
-              <h1 className="w-[366px] text-[24px] text-center">
-                create a Sproutify account and enjoy amazing services we offer
-              </h1>
-              <Link href={"/sign-up"}>
-                <button className="py-[13px] px-[32px] text-lg rounded-3xl w-[200px] bg-[#FFFFFF] text-[#4C2884] font-bold">
-                  Sign Up
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="my-16 w-[800px] flex min-h-[calc(100vh-135px)] flex-col items-center justify-center px-[5%] lg:my-20">
-          <h1 className="mb-10 text-3xl">Sign In</h1>
-
+      <section
+        className=" flex justify-evenly lg:flex-row flex-col "
+        style={{
+          backgroundImage: `url(${Polygon4.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "right",
+        }}
+      >
+        <div className="my-16 flex min-h-[calc(100vh-135px)] flex-col items-center justify-center px-[5%] lg:my-20">
           <form
             className="mb-4 mt-4 flex w-full max-w-[470px] flex-col items-center rounded-3xl border border-[#D9D9D9] px-8 pt-12 pb-8 lg:px-[72px]"
             onChange={handleInputChange}
           >
-            <h2 className="mb-10 text-center text-2xl">
-              Input sign in information
-            </h2>
+            <h1 className="mb-10 text-3xl">Sign In</h1>
             <div className="w-full">
               <input
                 className="mb-8 w-full rounded-xl border py-4 px-3 leading-tight text-gray-700 focus:shadow"
@@ -84,7 +70,7 @@ function Signin() {
 
               <button
                 className={`w-full rounded-xl  ${
-                  isFormFilled ? "bg-[#4C2884]" : "bg-[#9A4CEC]"
+                  isFormFilled ? "bg-black" : "bg-[#A0A0A0]"
                 } py-4 px-4 text-lg font-bold text-white`}
                 type="button"
                 disabled={isFormFilled}
@@ -92,14 +78,14 @@ function Signin() {
                 Sign In
               </button>
 
-              <div className="mt-6 flex w-full flex-row justify-evenly">
+              {/* <div className="mt-6 flex w-full flex-row justify-evenly">
                 <p className="text-center font-sans text-base text-[#1F1717]">
                   You do not have an account?{" "}
                   <a className="text-[#00820D]" href="#">
                     Sign Up
                   </a>
                 </p>
-              </div>
+              </div> */}
 
               <div className="mt-8 flex justify-center gap-x-6">
                 <button>
@@ -112,7 +98,20 @@ function Signin() {
             </div>
           </form>
         </div>
-      </div>
+
+        <div className="lg:w-[400px] w-full mb-10 flex items-center text-center justify-center flex-col text-xl lg:text-[24px] lg:font-medium gap-5 lg:text-black text-[#FFFFFF]">
+          <h1>Don't have an account?</h1>
+          <h1>Lorem ipsum dolor sit amet consectetur. Faucibus sagittis.</h1>
+          <Link href={"/signup"}>
+            <button
+              className="py-[13px] px-[32px] text-lg text-white rounded-xl w-[200px] bg-[#1F1717] font-medium  shadow-2xl"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
