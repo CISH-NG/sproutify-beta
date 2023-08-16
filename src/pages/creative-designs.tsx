183fe2fdcd6f9dec418d2461877e4a68aef3d35c
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-08-16 10:22:18 +0100

    WIP: Implementing creative design

--- src/pages/creative-designs.tsx
+++ src/pages/creative-designs.tsx
@@ -2,7 +2,7 @@ import Screens from "@/components/Screens/Screens";
 import DefaultLayout from "@/layouts/DefaultLayouts";
 import React from "react";
 
-export default function websites() {
+export default function CreativeDesigns() {
   return (
     <DefaultLayout>
       <Screens />;
