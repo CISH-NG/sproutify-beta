fc2e9a36f4086094f6ff77cf33c5c8dcd8db0976
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-08-16 10:09:14 +0100

    WIP: Implementing the Ui- for creative designs

---
+++ src/pages/creative-designs.tsx
@@ -0,0 +1,11 @@
+import Screens from "@/components/Screens/Screens";
+import DefaultLayout from "@/layouts/DefaultLayouts";
+import React from "react";
+
+export default function websites() {
+  return (
+    <DefaultLayout>
+      <Screens />;
+    </DefaultLayout>
+  );
+}
